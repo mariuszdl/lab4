5f9aa92722961e567d246029fba3f0f101b2aac9
Mariusz Dłubak <maniek.14.12.2@gmail.com>
2022-05-04 09:55:43 +0200

    Utworzenie handlera rysowania

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab4/Pong1.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab4/Pong1.java
@@ -1,5 +1,7 @@
 package pl.lublin.wsei.java.cwiczenia.lab4;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,6 +10,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -24,22 +27,32 @@ public class Pong1 extends Application {
     private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
     private static final double R = 10;
 
+    private double x = ARENAX1 + ARENAWIDTH / 2;
+
     @Override
     public void start(Stage stage) {
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Timeline t = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
+        t.setCycleCount(Timeline.INDEFINITE);
+
         stage.setTitle("Kulki");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
-        gc.setFill(Color.BLACK);
-        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
-
-        gc.setFill(Color.WHITESMOKE);
-        gc.fillOval(ARENAX1 + ARENAWIDTH / 2, ARENAY1 + ARENAHEIGHT / 2, 2 * R, 2 * R);
+        t.play();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    private void run(GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
+
+        gc.setFill(Color.WHITESMOKE);
+        gc.fillOval(x += 5, ARENAY1 + ARENAHEIGHT / 2, 2 * R, 2 * R);
     }
 }
